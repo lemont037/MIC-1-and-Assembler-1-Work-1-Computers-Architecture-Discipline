8e2a9d72aaadc52dcf7da0f3fa864acc3f7cacdd
marcosfmartin <44340351+marcosfmartin@users.noreply.github.com>
2019-05-06 20:13:55 -0300

    Update controle.cpp

--- controle.cpp
+++ controle.cpp
@@ -1,20 +1,31 @@
 #include "controle.hpp"
 
 
+typedef struct mnemonico
+{
+    string nome_mne;
+    byte mne_end;
+    int comprimento;
+};
+
+
 void controle(char* opcode)
 {   microcode rom [512];
     vector <byte> prog; //tem que ser vector pq não sabemos o tamanho do programa
     lerArquivos (prog, *rom);
+    vector <mnemonico> mnemonicos = criaMne();
     for (int i=4; prog.size();i++) //começa do byte 5 pq os 4 primeiros é só o tamanho do programa
     {
-        processador(rom [prog[i]]); //na minha cabeça, o microprograma 
-                                    //pega o endereço da instrução no programa e executa usando o processador
-                                    //não precisa se preocupar com os bytes de endereços de variáveis etc, pq o proprio microprograma já cuida disso
-        
+        processador(rom [prog[i]]); //na minha cabeça, executa o microprograma pega o endereço da instrução no programa e executa usando o processador
+        i = i+procuraMnemonico(prog[i], mnemonicos) //precisamos avançar o comprimento do mnemonico nos bytes do programa quando usamos variaveis etc
+
+
     }
 
 
 }
+
+
 
 void lerArquivos (vector<byte> &prog, microcode &rom)
 {
@@ -37,4 +48,104 @@ void lerArquivos (vector<byte> &prog, microcode &rom)
     }
 
 }
+
+int procuraMnemonico(byte endereco, vector<mnemonico> mnemonicos) //retorna comprimento do mnemonico que tá sendo executado no microprograma
+{
+    for (int i=0; i<mnemonicos.size(); i++)
+    {
+        if (endereco == mnemonicos[i].mne_end)
+        {
+            return mnemonicos[i].comprimento;
+        }
+    }
+
+}
+
+vector <mnemonico> criaMne ()
+{
+    vector <mnemonico> mnemonicos;
+    mnemonico tmp;
+    tmp.nome_mne = "nop";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x01;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iadd";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x02;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "isub";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x05;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iand";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x08;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "ior";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x0B;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "dup";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x0E;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "pop";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x10;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "swap";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x13;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "bipush";
+    tmp.comprimento = 2;
+    tmp.mne_end=0x19;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iload";
+    tmp.comprimento = 2;
+    tmp.mne_end=0x1C;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "istore";
+    tmp.comprimento = 2;
+    tmp.mne_end=0x22;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "wide";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x28;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "ldc_w";
+    tmp.comprimento = 2;
+    tmp.mne_end=0x32;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iinc";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x36;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "goto";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x3C;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iflt";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x43;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "ifeq";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x47;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "if_icmpeq";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x4B;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "invokevirtual";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x55;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "ireturn";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x6B;
+    mnemonicos.push_back(tmp);
+    return mnemonicos;
+}
+
 
