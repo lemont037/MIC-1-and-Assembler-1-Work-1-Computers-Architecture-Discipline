d833ab2dc005437734932f5370286dd0f41e4466
marcosfmartin <44340351+marcosfmartin@users.noreply.github.com>
2019-05-06 19:42:08 -0300

    Add files via upload

---
+++ controle.cpp
@@ -0,0 +1,130 @@
+#include "controle.hpp"
+
+
+typedef struct mnemonico
+{
+    string nome_mne;
+    byte mne_end;
+    int comprimento;
+};
+
+
+void controle(char* opcode)
+{   microcode rom [512];
+    vector <byte> prog; //tem que ser vector pq não sabemos o tamanho do programa
+    lerArquivos (prog, *rom);
+    vector <mnemonico> mnemonicos = criaMne();
+
+
+
+}
+
+void lerArquivos (vector<byte> &prog, microcode &rom)
+{
+    ifstream ReadMicro;
+    ifstream ReadProg;
+    ReadMicro.open("microprog.rom", ios::binary);
+    ReadProg.open("progEmulado2.exe", ios::binary);
+
+    ReadMicro.read((char*)&rom, 512*sizeof(microcode));//ler microprog
+    byte atual;
+    while (true)
+    {
+        ReadProg >> atual;
+        if (!ReadProg.good()) // se não fizer assim, ele repete o ultimo byte
+        {
+            break;
+        }
+
+        prog.push_back(atual);
+    }
+
+}
+
+vector <mnemonico> criaMne ()
+{
+    vector <mnemonico> mnemonicos;
+    mnemonico tmp;
+    tmp.nome_mne = "nop";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x01;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iadd";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x02;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "isub";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x05;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iand";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x08;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "ior";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x0B;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "dup";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x0E;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "pop";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x10;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "swap";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x13;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "bipush";
+    tmp.comprimento = 2;
+    tmp.mne_end=0x19;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iload";
+    tmp.comprimento = 2;
+    tmp.mne_end=0x1C;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "istore";
+    tmp.comprimento = 2;
+    tmp.mne_end=0x22;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "wide";
+    tmp.comprimento = 1;
+    tmp.mne_end=0x28;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "ldc_w";
+    tmp.comprimento = 2;
+    tmp.mne_end=0x32;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iinc";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x36;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "goto";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x3C;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "iflt";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x43;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "ifeq";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x47;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "if_icmpeq";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x4B;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "invokevirtual";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x55;
+    mnemonicos.push_back(tmp);
+    tmp.nome_mne = "ireturn";
+    tmp.comprimento = 3;
+    tmp.mne_end=0x6B;
+    mnemonicos.push_back(tmp);
+    return mnemonicos;
+}
+
---
+++ controle.hpp
@@ -0,0 +1,23 @@
+#ifndef CONTROLE_HPP_INCLUDED
+#define CONTROLE_HPP_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+typedef unsigned int word;
+typedef unsigned char byte;
+typedef unsigned long long microcode;
+
+
+
+void controle(char* opcode);
+void lerArquivos (vector<byte> &prog, microcode &rom);
+void lerByte (atual)
+
+#endif // CONTROLE_HPP_INCLUDED
